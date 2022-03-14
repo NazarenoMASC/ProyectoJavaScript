b98295cdc9f9621d34ad1a9d72b70e67c021abe7
Nazareno Silva <nma.silvacordero@gmail.com>
2022-03-14 19:06:24 -0300

    incorporar eventos

--- js/main.js
+++ js/main.js
@@ -1,53 +1,33 @@
-let n1;
-let n2;
-let operacion = 0;
+const addedCards = document.querySelectorAll(".cards");
 
-const sumar = (n1, n2) => {
-  return parseFloat(n1) + parseFloat(n2);
-};
-const restar = (n1, n2) => {
-  return parseFloat(n1) - parseFloat(n2);
-};
-const concatenar = (n1, n2) => {
-  return (resultado = n1.concat(n2));
-};
-const dividir = (n1, n2) => {
-  return parseFloat(n1) / parseFloat(n2);
-};
-const CalcularPorcentaje = (n1, n2) => {
-  return (n1 * 100) / n2;
-};
+addedCards.forEach((addButton) => {
+  addButton.addEventListener("click", addToCart);
+});
+const cartCardsAdded = document.querySelector(".cart-container");
 
-while (operacion !== "n") {
-  alert("Que operacion desea realizar?");
-  operacion = prompt(
-    "1: suma, 2: resta, 3: concatenar, 4: dividir, 5: Sacar el porcentaje. <br> Presione n para cancelar"
-  );
+function addToCart(event) {
+  const button = event.target;
+  const card = button.closest(".cards");
 
-  if ((operacion == 3) & (operacion !== "n")) {
-    n1 = prompt("Ingrese primer palabra");
-    n2 = prompt("Ingrese segunda palabra");
+  const cardTitle = card.querySelector("h3").textContent;
+  const cardPrice = card.querySelector("p").textContent;
+  const cardImg = card.querySelector("img").src;
 
-    alert(` el resultado es ` + concatenar(n1, n2));
-  } else if ((operacion == 5) & (operacion !== "n")) {
-    n1 = prompt("Ingrese numero");
-    n2 = prompt("Ingrese el porcentaje de ese numero");
-    let resultado = CalcularPorcentaje(n1, n2);
-    alert(` el ${n2}% de ${n1} es ${resultado}`);
-  } else if ((operacion == 1) & (operacion !== "n")) {
-    n1 = prompt("Ingrese primer numero");
-    n2 = prompt("Ingrese segundo numero");
-    let resultado = sumar(n1, n2);
-    alert(` el resultado de la suma es ${resultado}`);
-  } else if ((operacion == 2) & (operacion !== "n")) {
-    n1 = prompt("Ingrese primer numero");
-    n2 = prompt("Ingrese segundo numero");
-    let resultado = restar(n1, n2);
-    alert(` el resultado de la resta es ${resultado}`);
-  } else if ((operacion == 4) & (operacion !== "n")) {
-    n1 = prompt("Ingrese primer numero");
-    n2 = prompt("Ingrese segundo numero");
-    let resultado = dividir(n1, n2);
-    alert(` el resultado de la división es ${resultado}`);
-  }
+  addCardToCart(cardTitle, cardPrice, cardImg);
+}
+
+function addCardToCart(cardTitle, cardPrice, cardImg) {
+  const cartDiv = document.createElement("div");
+
+  const divCreatedCart = `
+            <div class="cart-flex-row">
+                <h3> ${cardTitle} </h3>
+                <img src=${cardImg} alt="imagen">
+                <p> ${cardPrice} </p>
+            </div>
+
+       
+            `;
+  cartDiv.innerHTML = divCreatedCart;
+  cartCardsAdded.appendChild(cartDiv);
 }
